fa0a8b768b02218601ee882d1bca1f11cc815388
Rajat131193 <35720457+Rajat131193@users.noreply.github.com>
2018-03-26 13:13:31 +0530

    Update EC2_Stop_time.sh

--- EC2_Stop_time.sh
+++ EC2_Stop_time.sh
@@ -11,4 +11,5 @@ DATE=`date +%Y-%m-%d`
 DATE1=$(date --date="${DATE} -${No_of_Days} day" +%Y-%m-%d)
 
 #Print EC2 instances with stopped state from last n no of days
+echo "Below is the ec2 instances with stopped state from last ${No_of_Days} days : "
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped" "Name=launch-time,Values=${DATE1}*" --query 'Reservations[].Instances[].[InstanceId,LaunchTime]' --output text
