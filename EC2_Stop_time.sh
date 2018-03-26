be7dcb7049e19efc07480ef10964a8f67a21e22a
Rajat131193 <35720457+Rajat131193@users.noreply.github.com>
2018-03-26 13:19:56 +0530

    Update EC2_Stop_time.sh

--- EC2_Stop_time.sh
+++ EC2_Stop_time.sh
@@ -6,10 +6,10 @@ echo "Please enter the no of days"
 read No_of_Days
 
 #Storing curent date in YY-MM-DD format
-DATE=`date +%Y-%m-%d`
+CURRENT_DATE=`date +%Y-%m-%d`
 #Subtarcting no of days from current date
-DATE1=$(date --date="${DATE} -${No_of_Days} day" +%Y-%m-%d)
+FINAL_DATE=$(date --date="${CURRENT_DATE} -${No_of_Days} day" +%Y-%m-%d)
 
 #Print EC2 instances with stopped state from last n no of days
 echo "Below is the ec2 instances with stopped state from last ${No_of_Days} days : "
-aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped" "Name=launch-time,Values=${DATE1}*" --query 'Reservations[].Instances[].[InstanceId,LaunchTime]' --output text
+aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped" "Name=launch-time,Values=${FINAL_DATE}*" --query 'Reservations[].Instances[].[InstanceId,LaunchTime]' --output text
